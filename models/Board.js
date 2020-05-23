58195a8328edaaa8ab53a0dfd27bae3b1da23961
whdudtod1273 <whdudtod1273@naver.com>
2020-05-23 15:27:55 +0900

    user,board모델작성

---
+++ models/Board.js
@@ -0,0 +1,19 @@
+
+module.exports = function (Sequelize, DataTypes) {
+
+    var Board = Sequelize.define('Board', {
+
+        id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+        title: { type: DataTypes.STRING },
+        description: { type: DataTypes.TEXT },
+        writer: { type: DataTypes.STRING },
+        like: { type: DataTypes.INTEGER },
+    });
+    Board.associate = (models) => {
+        Products.belongsTo(
+            models.User,
+            { as: 'Owner', foreignKey: 'user_id', targetKey: 'id' }
+        );
+    }
+    return Board
+}
---
+++ models/User.js
@@ -0,0 +1,15 @@
+const passwordHash = require('../helpers/passwordHash');
+
+module.exports = function (Sequelize, DataTypes) {
+    var User = Sequelize.define('User', {
+        email: { type: DataTypes.STRING },
+        password: { type: DataTypes.STRING },
+        username: { type: DataTypes.STRING },
+        phone: { type: DataTypes.STRING }
+    });
+
+    User.beforeCreate((user, _) => {
+        user.password = passwordHash(user.password);
+    });
+    return User
+}
