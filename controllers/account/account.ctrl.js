65b9b9830b7ff8b115545a9b20d1d61f8bc427c1
BaeDaBin <dabin@albacheck.com>
2020-05-25 14:02:00 +0900

    회원가입 중복 체크, 로그인 비밀번호체크 추가

--- controllers/account/account.ctrl.js
+++ controllers/account/account.ctrl.js
@@ -1,31 +1,44 @@
 const models = require('../../models')
+const passwordHash = require('../../helpers/passwordHash');
 
 exports.post_join = (req, res) => {
-  // res.send(req.body)
-  models.User.create(req.body).then(() => {
-    res.send(200, req.body)
-  })
-}
-
-exports.post_login = async (req, res) => {
-  let data
-  try {
-    const response = await models.User.findOne({
-      where: { email: req.body.email }
-    })
-    data = response
-  } catch (error) {
-    data = error.response
-    // res.send(error)
-
-  } finally {
-    if (data) {
-      res.send(200, data)
-    } else {
-      res.send(404, data)
-    }
+  if (!req.body.email || !req.body.password || !req.body.username) {
+    res.send(400, '입력을 제대로 하지 않았습니다.')
+    return
   }
 
+  const createUser = () => {
+    models.User.create(req.body).then(() => {
+      res.send(200, req.body)
+    })
+  }
 
+  models.User.findOne({ where: { email: req.body.email } })
+    .then(data => {
+      if (data) {
+        res.send(400, '이미 가입된 이메일이 있습니다.')
+      } else {
+        createUser()
+      }
+    })
+    .catch(error => res.send(error))
+}
+
+exports.post_login = (req, res) => {
+  models.User.findOne({ where: { email: req.body.email } })
+    .then(data => {
+      if (data) {
+        if (data.password === passwordHash(req.body.password)) {
+          res.send(200, '로그인 성공')
+        } else {
+          res.send(400, '비밀번호가 일치하지 않습니다')
+        }
+      } else {
+        res.send(400, '회원을 찾을 수 없습니다.')
+      }
+    })
+    .catch(error => {
+      res.send(error)
+    })
 }
 
